185534798eac1e383fc0847ffc2d4ca2e1bbfe0e
Antônio Zanotti <antoniorzanotti@gmail.com>
2024-04-17 12:53:33 -0300

    feat: create components files

---
+++ components/ui/Img.vue
@@ -0,0 +1,5 @@
+<template>
+  <header>
+    <img src="/images/kotas-logo.svg" alt="Kotas Logo" width="80" height="24" />
+  </header>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="layout">
-    <AppHeader />
+    <CommonHeader />
     <slot />
   </div>
 </template>
