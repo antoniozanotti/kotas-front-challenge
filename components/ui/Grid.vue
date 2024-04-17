db0206c214ca329d29915e5ba61d9868a21991be
Antônio Zanotti <antoniorzanotti@gmail.com>
2024-04-17 18:40:11 -0300

    feat: responsive grid

--- components/ui/Grid.vue
+++ components/ui/Grid.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 justify-items-center gap-y-4 sm:gap-y-6 md:gap-y-8">
+  <div class="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-x-6 lg:gap-x-4 gap-y-4 lg:gap-y-8">
     <slot />
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,13 @@
 <template>
-  <UiSearch />
-  <h1>Pokémons</h1>
-  <UiGrid>
-    <PagesListingPokemonCard
-      v-for="item in [...Array(24).keys()]"
-      :pokemon="{id: 4, name: 'Charmander', types: ['fire', 'water']}"
-    />
-  </UiGrid>
+  <div class="w-[330px] sm:w-[507px] lg:w-[829px] mx-auto">
+    <UiSearch />
+    <h1 class="font-bold text-[14px] leading-[18px] pt-[49px] pb-[36px]">Pokémons</h1>
+    <UiGrid>
+      <PagesListingPokemonCard
+        v-for="item in [...Array(24).keys()]"
+        :pokemon="{id: 4, name: 'Charmander', types: ['fire', 'water']}"
+      />
+    </UiGrid>
+  </div>
   <UiScroll />
 </template>
