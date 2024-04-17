c8cf408600102e6cbdb0c43028c5a6a2abf35c7b
Antônio Zanotti <antoniorzanotti@gmail.com>
2024-04-17 14:17:47 -0300

    feat: set upboxShadow and header component

--- components/common/Header.vue
+++ components/common/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <header>
-    <img src="/images/kotas-logo.svg" alt="Kotas Logo" width="80" height="24" />
+  <header class="shadow-sm-c1 py-3 px-4 text-center md:text-left">
+    <img src="/images/kotas-logo.svg" alt="Kotas Logo" width="80" height="24" class="inline" />
   </header>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,11 @@ export default {
         'primary': ['Mulish'],
         'secondary': ['Quicksand'],
       },
+      boxShadow: {
+        'sm-c1': '0 1px 1px 0 #E1E1E1',
+        'c1': '0 4px 20px 0 #E1E1E1',
+        'md-c1': '0 4px 20px 0 rgba(0, 0, 0, 0.05)',
+      }
     },
   },
   plugins: [],
