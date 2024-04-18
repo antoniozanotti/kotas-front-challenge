2d46c6722b493440c85878a47ad95104098e4a50
Antônio Zanotti <antoniorzanotti@gmail.com>
2024-04-18 17:48:31 -0300

    feat: fetch data from api for pokemon page

--- pages/pokemon/[slug].vue
+++ pages/pokemon/[slug].vue
@@ -1,9 +1,37 @@
+<script lang="ts" setup>
+import { useQuery } from "@tanstack/vue-query";
+
+const fetchPokemon = async (name: string) => {
+  let fetchUrl = `https://pokeapi.co/api/v2/pokemon/${name}`;
+  const res = await fetch(fetchUrl);
+  return res.json();
+};
+
+const route = useRoute();
+const pokemonName = route.params.slug.toString();
+
+const { isPending, data } = useQuery({
+  queryKey: ["pokemon", pokemonName],
+  queryFn: () => fetchPokemon(pokemonName),
+});
+</script>
 <template>
   <div class="md:w-[541px] mx-auto">
-    <PagesPokemonHeader
-      :pokemon="{ id: 4, name: 'aaa', types: ['aaa', 'bbb'] }"
-    />
-    <PagesPokemonAbilities :abilities="['afd', 'asdf', 'ccc', 'ddd']" />
-    <NuxtLink to="/" class="text-[#00A3FF] font-bold text-sm leading-[41px] h-[41px] w-[100px] block mx-auto text-center mt-[48px]">Voltar</NuxtLink>
+    <span v-if="isPending"></span>
+    <div v-else>
+      <PagesPokemonHeader
+        :pokemon="{ id: data.id, name: data.name, types: data.types.map((pokemonType:any) => {return pokemonType.type.name;}) }"
+      />
+      <PagesPokemonAbilities
+        :abilities="data.abilities.map((ability:any) => {
+          return ability.ability.name;
+        })"
+      />
+    </div>
+    <NuxtLink
+      to="/"
+      class="text-[#00A3FF] font-bold text-sm leading-[41px] h-[41px] w-[100px] block mx-auto text-center mt-[48px]"
+      >Voltar</NuxtLink
+    >
   </div>
 </template>
